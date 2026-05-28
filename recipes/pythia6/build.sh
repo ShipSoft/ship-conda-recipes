43a504582662d462f53362a8e448a02ad9a4b128
Oliver Lantwin <oliver.lantwin@cern.ch>
2026-05-29 02:40:47 +0700

    refactor(pythia6): use git source and patch file
    Switch from archive URL to git source and replace sed-based patching of
    Fortran subroutine names with a proper patch file applied by rattler-build.

--- recipes/pythia6/build.sh
+++ recipes/pythia6/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash -e
-# Restore dummy PDFLIB subroutines (renamed to *X in this fork).
-# The real ones come from PDFLIB which we don't use; without the dummies,
-# the symbols are undefined and dlopen fails (conda-forge uses BIND_NOW).
-sed -i -e 's/SUBROUTINE PDFSETX/SUBROUTINE PDFSET/' \
-       -e 's/SUBROUTINE STRUCTPX/SUBROUTINE STRUCTP/' \
-       -e 's/SUBROUTINE STRUCTMX/SUBROUTINE STRUCTM/' "${SRC_DIR}/pythia-6.4.28.f"
-
 mkdir -p build && cd build
 cmake ${CMAKE_ARGS} ${SRC_DIR} \
     -DCMAKE_BUILD_TYPE=Release \
