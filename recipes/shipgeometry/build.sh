0507ef3b9f52bc1279803812746b594cee8cd748
Oliver Lantwin <oliver.lantwin@cern.ch>
2026-06-18 12:37:58 +0100

    recipes(shipgeometry): bump to v0.2.1; drop CMAKE_INSTALL_DATADIR workaround
    Upstream v0.2.1 hoists include(GNUInstallDirs) before
    add_subdirectory(subsystems), so subsystem install() rules see a sane
    CMAKE_INSTALL_DATADIR without the explicit -DCMAKE_INSTALL_DATADIR=share
    flag added in f23abd0. Also test the new TimingDetector GMX data file
    that v0.2.1 installs under share/SHiPGeometry/TimingDetector/.

--- recipes/shipgeometry/build.sh
+++ recipes/shipgeometry/build.sh
@@ -3,7 +3,6 @@ set -euxo pipefail
 cmake ${CMAKE_ARGS} -S ${SRC_DIR} -B build \
     -DCMAKE_CXX_STANDARD=20 \
     -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_INSTALL_DATADIR=share \
     -DBUILD_SHARED_LIBS=ON \
     -DBUILD_TESTING=OFF
 cmake --build build --parallel ${CPU_COUNT}
