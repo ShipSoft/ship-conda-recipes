e0b0e877a735586c87844589dc946a6ab08c5c7d
Oliver Lantwin <oliver.lantwin@cern.ch>
2026-07-02 16:40:12 +0200

    style: fix lint issues surfaced by the prek migration
    Wrap an over-long jq table-row line in the channel-snapshot workflow (using
    jq string concatenation, identical output) and silence SC2154 for
    root_cxx_standard, which is injected by the build environment.

--- recipes/acts-ship/build.sh
+++ recipes/acts-ship/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
+# shellcheck disable=SC2154  # root_cxx_standard is injected by the build environment
 cmake ${CMAKE_ARGS} -S "${SRC_DIR}" -B build \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_LIBDIR=lib \
--- recipes/shipdatamodel/build.sh
+++ recipes/shipdatamodel/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
+# shellcheck disable=SC2154  # root_cxx_standard is injected by the build environment
 cmake ${CMAKE_ARGS} -S ${SRC_DIR} -B build \
     -DCMAKE_CXX_STANDARD=${root_cxx_standard} \
     -DCMAKE_INSTALL_LIBDIR=lib
